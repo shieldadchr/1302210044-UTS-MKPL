6a044ef2fad89adc1b53348d709db2f6bfbb0699
shieldadchr <shielda.diasavitri@gmail.com>
2024-04-21 16:20:16 +0700

    Meningkatkan keterbacaan dan pemeliharaan kode dengan mengganti angka-angka tetap menjadi konstanta, memisahkan logika perhitungan pajak ke dalam metode terpisah, menyederhanakan kondisi, dan mempertimbangkan penggunaan objek terpisah untuk mengelola parameter perhitungan pajak.

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,7 +1,6 @@
 package lib;
 
 public class TaxFunction {
-    
     public static class TaxCalculationInfo {
         private int monthlySalary;
         private int otherMonthlyIncome;
@@ -9,8 +8,7 @@ public class TaxFunction {
         private int deductible;
         private boolean isMarried;
         private int numberOfChildren;
-        
-        // Constructor
+
         public TaxCalculationInfo(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
             this.monthlySalary = monthlySalary;
             this.otherMonthlyIncome = otherMonthlyIncome;
@@ -19,61 +17,58 @@ public class TaxFunction {
             this.isMarried = isMarried;
             this.numberOfChildren = numberOfChildren;
         }
-        
-        // Getter methods
+
         public int getMonthlySalary() {
             return monthlySalary;
         }
-        
+
         public int getOtherMonthlyIncome() {
             return otherMonthlyIncome;
         }
-        
+
         public int getNumberOfMonthWorking() {
             return numberOfMonthWorking;
         }
-        
+
         public int getDeductible() {
             return deductible;
         }
-        
+
         public boolean isMarried() {
             return isMarried;
         }
-        
+
         public int getNumberOfChildren() {
             return numberOfChildren;
         }
     }
-    
+
+    public static final int MAX_WORKING_MONTHS_PER_YEAR = 12;
+    public static final int MAX_CHILDREN_FOR_TAX = 3;
+    public static final int STANDARD_DEDUCTIBLE = 54000000;
+    public static final int PER_CHILD_DEDUCTIBLE = 1500000;
+
     public static int calculateTax(TaxCalculationInfo taxInfo) {
-        int tax = 0;
+        validateTaxInfo(taxInfo);
+
         int monthlySalary = taxInfo.getMonthlySalary();
         int otherMonthlyIncome = taxInfo.getOtherMonthlyIncome();
         int numberOfMonthWorking = taxInfo.getNumberOfMonthWorking();
         int deductible = taxInfo.getDeductible();
         boolean isMarried = taxInfo.isMarried();
-        int numberOfChildren = taxInfo.getNumberOfChildren();
+        int numberOfChildren = Math.min(taxInfo.getNumberOfChildren(), MAX_CHILDREN_FOR_TAX);
+
+        int totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+        int totalDeductible = deductible + (isMarried ? 0 : STANDARD_DEDUCTIBLE) + Math.min(numberOfChildren * PER_CHILD_DEDUCTIBLE, MAX_CHILDREN_FOR_TAX * PER_CHILD_DEDUCTIBLE);
+        int taxableIncome = Math.max(0, totalIncome - totalDeductible);
+        int tax = (int) Math.round(0.05 * taxableIncome);
         
-        // Calculation logic
-        if (numberOfMonthWorking > 12) {
-            System.err.println("More than 12 month working per year");
-        }
-        
-        if (numberOfChildren > 3) {
-            numberOfChildren = 3;
-        }
-        
-        if (isMarried) {
-            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-        } else {
-            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-        }
-        
-        if (tax < 0) {
-            return 0;
-        } else {
-            return tax;
+        return tax;
+    }
+
+    private static void validateTaxInfo(TaxCalculationInfo taxInfo) {
+        if (taxInfo.getNumberOfMonthWorking() > MAX_WORKING_MONTHS_PER_YEAR) {
+            throw new IllegalArgumentException("Lebih dari 12 bulan bekerja dalam setahun");
         }
     }
 }
