28e38c6696995a407bcb40228084b486f749d313
shieldadchr <shielda.diasavitri@gmail.com>
2024-04-21 16:50:33 +0700

    mengubah kelas Employee: ekstraksi konstan, penggabungan fragmen kondisional, penggantian parameter boolean dengan enum, pengenalan objek parameter, dan pemisahan perhitungan bulan kerja

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -12,13 +12,9 @@ public class Employee {
     private String idNumber;
     private String address;
 
-    private int yearJoined;
-    private int monthJoined;
-    private int dayJoined;
-    private int monthWorkingInYear;
-
+    private LocalDate dateJoined;
     private boolean isForeigner;
-    private boolean gender; //true = Laki-laki, false = Perempuan
+    private Gender gender;
 
     private int monthlySalary;
     private int otherMonthlyIncome;
@@ -27,37 +23,34 @@ public class Employee {
     private String spouseName;
     private List<String> childNames;
 
-    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, LocalDate dateJoined, boolean isForeigner, Gender gender) {
         this.employeeId = employeeId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNumber = idNumber;
         this.address = address;
-        this.yearJoined = yearJoined;
-        this.monthJoined = monthJoined;
-        this.dayJoined = dayJoined;
+        this.dateJoined = dateJoined;
         this.isForeigner = isForeigner;
         this.gender = gender;
 
-        this.childNames = new LinkedList<String>();
+        this.childNames = new LinkedList<>();
     }
 
-    public void setMonthlySalary(int grade) {
-        if (grade == 1) {
-            monthlySalary = 3000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
-        } else if (grade == 2) {
-            monthlySalary = 5000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
-        } else if (grade == 3) {
-            monthlySalary = 7000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
+    public void setMonthlySalary(Grade grade) {
+        switch (grade) {
+            case GRADE_1:
+                monthlySalary = Constants.GRADE_1_SALARY;
+                break;
+            case GRADE_2:
+                monthlySalary = Constants.GRADE_2_SALARY;
+                break;
+            case GRADE_3:
+                monthlySalary = Constants.GRADE_3_SALARY;
+                break;
+        }
+
+        if (isForeigner) {
+            monthlySalary *= Constants.FOREIGNER_MULTIPLIER;
         }
     }
 
@@ -78,21 +71,42 @@ public class Employee {
     }
 
     public int getAnnualIncomeTax() {
-        // Menghitung berapa lama pegawai bekerja dalam setahun ini
+        int monthWorkingInYear = calculateMonthsWorkedInYear();
+
+        TaxFunction.TaxCalculationInfo taxInfo = new TaxFunction.TaxCalculationInfo(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseName != null, childNames.size());
+
+        return TaxFunction.calculateTax(taxInfo);
+    }
+
+    private int calculateMonthsWorkedInYear() {
         LocalDate currentDate = LocalDate.now();
         int currentYear = currentDate.getYear();
         int currentMonth = currentDate.getMonthValue();
 
-        if (currentYear == yearJoined) {
-            monthWorkingInYear = currentMonth - monthJoined;
+        int monthWorkingInYear;
+        if (currentYear == dateJoined.getYear()) {
+            monthWorkingInYear = currentMonth - dateJoined.getMonthValue();
         } else {
             monthWorkingInYear = 12;
         }
-
-        // Inisialisasi objek TaxCalculationInfo
-        TaxFunction.TaxCalculationInfo taxInfo = new TaxFunction.TaxCalculationInfo(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseName != null, childNames.size());
-
-        // Memanggil metode calculateTax dengan objek taxInfo sebagai parameter
-        return TaxFunction.calculateTax(taxInfo);
+        return monthWorkingInYear;
     }
-} 
+
+    private static class Constants {
+        static final int GRADE_1_SALARY = 3000000;
+        static final int GRADE_2_SALARY = 5000000;
+        static final int GRADE_3_SALARY = 7000000;
+        static final double FOREIGNER_MULTIPLIER = 1.5;
+    }
+
+    public enum Grade {
+        GRADE_1,
+        GRADE_2,
+        GRADE_3
+    }
+
+    public enum Gender {
+        MALE,
+        FEMALE
+    }
+}
